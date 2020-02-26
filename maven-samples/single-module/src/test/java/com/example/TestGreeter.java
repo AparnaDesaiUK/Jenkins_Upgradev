b3a8f7e8e2173a8e4db90752c4ed6208e62c9189
Desai, Aparna <aparnadesaiuk@gmail.com>
2020-02-26 14:19:43 +0000

    chnages to testgreeter

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
   
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessageToAparna() {
     String someone = "Hello Aparna, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
